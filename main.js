68103e6fd7a5585eda70b7dd9ca243280b9e5060
Alvaro Miziara <alvaromiziara@Alvaros-MacBook-Air.local>
2023-12-29 13:31:40 +1000

    First commit through console

---
+++ main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var flkty = new Flickity('.container', {
+        wrapAround: true,
+        freeScroll: true,
+    });
+    console.log('Flickity initialized successfully');
+});
